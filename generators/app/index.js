a67d1c3182201488d1eff7ce227b260282b899df
limichange <limichange@hotmail.com>
2015-08-05 17:50:44 +0800

    npm init

---
+++ generators/app/index.js
@@ -0,0 +1,8 @@
+module.exports = generators.Base.extend({
+  method1: function () {
+    console.log('method 1 just ran');
+  },
+  method2: function () {
+    console.log('method 2 just ran');
+  }
+});
