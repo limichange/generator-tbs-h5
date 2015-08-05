bc88ccaffc5def5355b75b51b0f489d81dd02cc1
limichange <limichange@hotmail.com>
2015-08-05 18:06:59 +0800

    add template files

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,8 +1,71 @@
+// app/index.js
+var generators = require('yeoman-generator');
+
 module.exports = generators.Base.extend({
-  method1: function () {
-    console.log('method 1 just ran');
+
+  // 构造函数
+  constructor: function () {
+
+    // 调用父类构造函数
+    generators.Base.apply(this, arguments);
+
+    this.option('skip-install', {
+      desc: 'Skips the installation of dependencies',
+      type: Boolean
+    });
+
+    this.option('skip-install-message', {
+      desc: 'Skips the message after the installation of dependencies',
+      type: Boolean
+    });
+
   },
-  method2: function () {
-    console.log('method 2 just ran');
+
+  initializing: function () {
+    // this.pkg = require('./../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    var prompts = [{
+      type: 'checkbox',
+      name: 'features',
+      message: 'What more would you like?',
+      choices: [{
+        name: 'Sass',
+        value: 'includeSass',
+        checked: true
+      }, {
+        name: 'Bootstrap',
+        value: 'includeBootstrap',
+        checked: true
+      }, {
+        name: 'Jade',
+        value: 'includeJade',
+        checked: true
+      }, {
+        name: 'Modernizr',
+        value: 'includeModernizr',
+        checked: true
+      }]
+    }];
+
+    this.prompt(prompts, function (answers) {
+      var features = answers.features;
+
+      var hasFeature = function (feat) {
+        return features.indexOf(feat) !== -1;
+      };
+
+      // manually deal with the response, get back and store the results.
+      // we change a bit this way of doing to automatically do this in the self.prompt() method.
+      this.includeSass = hasFeature('includeSass');
+      this.includeBootstrap = hasFeature('includeBootstrap');
+      this.includeModernizr = hasFeature('includeModernizr');
+
+      done();
+    }.bind(this));
   }
+
 });
